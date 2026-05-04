4a3fad52239afc58d5e2e2fe2d4efa053601212a
coolrunner1 <153802775+coolrunner1@users.noreply.github.com>
2026-05-04 23:16:11 +0300

    Created a new function for displaying error messages

--- utils/utils.ts
+++ utils/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const getLengthErrorMessage = (locale: "en" | "ru", type: "min" | "max", size: number) => {
+  if (locale === "en") {
+    return `${type == 'min' ? "Minimum" : "Maximum"} length is ${size} symbols`;
+  }
+
+  return `Должен содержать ${type == 'min' ? "минимум" : "максимум"} ${size} символов`;
+}
